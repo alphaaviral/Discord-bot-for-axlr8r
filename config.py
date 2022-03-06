83d2027a5640f90f209f4d80c2d20577ecd83eff
alphaaviral <alphaaviral@gmail.com>
2022-03-06 18:15:13 +0530

    added clean final db, extra features

--- config.py
+++
@@ -1,3 +0,0 @@
-gmailpw = 'qkbwtmjluduokyya'
-emailid = 'alphaaviral@gmail.com'
-botpw = 'OTQ3NTc4NDMwMTk3NzQzNzA2.YhvTLg.h32zoBpopKJgi2W50Nw569OUP0A'
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import discord
 import pandas as pd
-import smtplib,ssl
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+# import smtplib,ssl
+# from email.mime.text import MIMEText
+# from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
+import requests
+import json
 
 load_dotenv()
 
@@ -12,11 +14,11 @@ intents = discord.Intents.default()
 intents.members = True
 intents.messages=True
 client = discord.Client(intents = intents)
-df = pd.read_csv("test.csv")
+df = pd.read_csv("data.csv")
 ids = df["Discord id (Don't have it? We would suggest you to make one)"].tolist()
 names = df["Name"].tolist()
 entrynos = df["Entry Number"].tolist()
-groups = df["group"].tolist()
+groups = df["Groups"].tolist()
 # print(df)
 @client.event
 async def on_ready():
@@ -28,82 +30,99 @@ async def on_message(message):
         return
 
     if message.content.startswith('$hello'):
-        await message.channel.send('Say hello!')
-        # role = discord.utils.get(message.guild.roles, name="member") #  Gets the member role as a `role` object
-        # await message.author.add_roles(role) # Gives the role to the user
+        await message.add_reaction("✅")
+        await message.delete(delay = 1)
 
-    if message.content.startswith('$send_invite'):
-        await create_invite(message)
+    # if message.content.startswith('$send_invite'):
+    #     await create_invite(message)
 
-    if message.content.startswith('??202') and (message.content.isupper() or message.content.islower()):
-        message_con = message.content[2:]
-        # print(groups)
-        for i in range(len(entrynos)):
-            if entrynos[i].casefold() == message_con.casefold():
-                # print(groups[i])
-                role = discord.utils.get(message.guild.roles, name="Group "+groups[i])
-                await message.author.add_roles(role)
-                await message.add_reaction("✅")
+    if message.content.startswith('??202'):
+        try:
+            if(len(message.author.roles)>1):
+                await message.author.send("You are already in a group")
+                await message.add_reaction("🛑")
                 await message.delete(delay = 2)
-                await message.author.edit(nick = names[i])
-                df.at[i, "Discord id (Don't have it? We would suggest you to make one)"] = message.author.name+"#"+message.author.discriminator
-                df.to_csv("test.csv", index=False)
-                break
+                print(len(message.author.roles))
+            else:
+                message_con = message.content[2:]
+                for i in range(len(entrynos)):
+                    if entrynos[i].casefold() == message_con.casefold():
+                        role = discord.utils.get(message.guild.roles, name="Group "+groups[i])
+                        fresherrole = discord.utils.get(message.guild.roles, name="Fresher")
+                        await message.author.add_roles(role, fresherrole)
+                        await message.add_reaction("✅")
+                        await message.delete(delay = 2)
+                        await message.author.send(content = "You have been assigned the group " + groups[i])
+                        await message.author.edit(nick = names[i])
+                        data = {
+                            'entry number': entrynos[i],
+                            'discord id': message.author.name+"#"+message.author.discriminator,
+                        }
+                        r = requests.post(url = "https://axlr8r-bot-default-rtdb.firebaseio.com/fresher-data.json", data =json.dumps(data))
+                        # print(r.status_code)
+                        return
+                await message.add_reaction("🛑")
+                await message.delete(delay = 2)
+                await message.author.send(content = "You entered an invalid entry number (You did not fill the form floated after the main orientation :). Contact a mod for more info!)")
+        except:
+            print("Error")
 
 @client.event
 async def on_member_join(member):
-    # print("ok")
-    # print("Recognised that a member called " + member.name + " joined")
-    # print(member.guild.roles)
-    # role = discord.utils.get(member.guild.roles, name="member") #  Gets the member role as a `role` object
-    
-    # print(ids)
-    # print(member.name)
-    nameanddis = member.name+"#"+member.discriminator
-    id = member.id
-    for i in range(len(ids)):
-        if type(ids[i])==str:
-            if ids[i].replace(" ", "").casefold() == nameanddis.replace(" ", "").casefold() or ids[i]==id or ids[i].replace(" ", "").casefold() == member.name.replace(" ", "").casefold():
-                role = discord.utils.get(member.guild.roles, name="Group "+groups[i])
-                await member.add_roles(role) # Gives the role to the user
-                await member.edit(nick= names[i])    
+    try:
+        nameanddis = member.name+"#"+member.discriminator
+        id = member.id
+        for i in range(len(ids)):
+            if type(ids[i])==str:
+                if ids[i].replace(" ", "").replace("@", "").replace("(","").replace(")", "").casefold() == nameanddis.replace(" ", "").casefold() or ids[i]==id or ids[i].replace(" ", "").replace("@", "").replace("(","").replace(")", "").casefold() == member.name.replace(" ", "").casefold():
+                    role = discord.utils.get(member.guild.roles, name="Group "+groups[i])
+                    fresherrole = discord.utils.get(member.guild.roles, name="Fresher")
+                    await member.add_roles(role, fresherrole) # Gives the role to the user
+                    await member.edit(nick= names[i])  
+                    data = {
+                            'entry number': entrynos[i],
+                            'discord id': member.name+"#"+member.discriminator,
+                        }
+                    r = requests.post(url = "https://axlr8r-bot-default-rtdb.firebaseio.com/fresher-data.json", data =json.dumps(data))
+                         
+    except:
+        print("error")
+# async def create_invite(message):
+#     guild = message.guild
+#     channel = guild.get_channel(947579944920313870)
+#     invite = await channel.create_invite(max_uses=1, temporary=True, unique=True, reason="Kerberos")
 
-async def create_invite(message):
-    guild = message.guild
-    channel = guild.get_channel(947579944920313870)
-    invite = await channel.create_invite(max_uses=1, temporary=True, unique=True, reason="Kerberos")
+#     port=465
+#     password= os.getenv("gmailpw")
+#     context = ssl.create_default_context()
+#     a = MIMEMultipart("alternative")
+#     a["Subject"] = "AXLR8R Formula Racing - Discord Invite"
+#     a["From"] = os.getenv("emailid")
+#     a["To"] = ""                                         #to email id
 
-    port=465
-    password= os.getenv("gmailpw")
-    context = ssl.create_default_context()
-    a = MIMEMultipart("alternative")
-    a["Subject"] = "AXLR8R Formula Racing - Discord Invite"
-    a["From"] = os.getenv("emailid")
-    a["To"] = ""                                         #to email id
+#    # Create the plain-text and HTML version of your message
+#     text = """\
+#     Hi,
+#     How are you?
+#     Real Python has many great tutorials:
+#     www.realpython.com"""
+#     html = """\
+#     <html>
+#     <body>
+#         <h2>Hi</h2>
+#         <h3>In reference to the google form you filled regarding recruitment for AXLR8R Formula Racing, here is your invite link for our discord server.
+#         Please note that this invite link is personalised for you(it only works once), hence we request you to not share it.</h3>
+#         <a href="{}">CLICK ME TO JOIN DISCORD!</a> 
+#         <img src="https://i.ibb.co/yQc7CHr/axlr8r-logo.png" alt="axlr8r-logo" border="0">
+#     </body>
+#     </html>
+#     """.format(invite.url)
+#     part1 = MIMEText(text, "plain")
+#     part2 = MIMEText(html, "html")
+#     a.attach(part1)
+#     a.attach(part2)
 
-   # Create the plain-text and HTML version of your message
-    text = """\
-    Hi,
-    How are you?
-    Real Python has many great tutorials:
-    www.realpython.com"""
-    html = """\
-    <html>
-    <body>
-        <h2>Hi</h2>
-        <h3>In reference to the google form you filled regarding recruitment for AXLR8R Formula Racing, here is your invite link for our discord server.
-        Please note that this invite link is personalised for you(it only works once), hence we request you to not share it.</h3>
-        <a href="{}">CLICK ME TO JOIN DISCORD!</a> 
-        <img src="https://i.ibb.co/yQc7CHr/axlr8r-logo.png" alt="axlr8r-logo" border="0">
-    </body>
-    </html>
-    """.format(invite.url)
-    part1 = MIMEText(text, "plain")
-    part2 = MIMEText(html, "html")
-    a.attach(part1)
-    a.attach(part2)
-
-    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        server.login("alphaaviral@gmail.com", password)
-        server.sendmail("alphaaviral@gmail.com","ph1200687@iitd.ac.in",a.as_string())
+#     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+#         server.login("alphaaviral@gmail.com", password)
+#         server.sendmail("alphaaviral@gmail.com","ph1200687@iitd.ac.in",a.as_string())
 client.run(os.getenv("botpw"))
